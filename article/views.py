b2aa16f453d44713f63fa4ffa8ba147d31a99346
weiqiang <weiqiang_1989@126.com>
2025-04-28 18:11:39 +0800

    end:第3讲 前端架构搭建

--- article/views.py
+++ article/views.py
@@ -8,10 +8,12 @@ from article.models import ArticleType
 # Create your views here.
 class TestView(View):
     def get(self, request):
-        articleTypeList_obj = ArticleType.objects.all()
-        print(articleTypeList_obj, type(articleTypeList_obj))
-        articleTypeList_dict = articleTypeList_obj._values()  # 转存字典
-        print(articleTypeList_dict, type(articleTypeList_dict))
-        articleTypeList = list(articleTypeList_dict)
-        print(articleTypeList, type(articleTypeList))
-        return JsonResponse({'code': 200, 'info': '测试！'})
+        # articleTypeList_obj = ArticleType.objects.all()
+        # print(articleTypeList_obj, type(articleTypeList_obj))
+        # articleTypeList_dict = articleTypeList_obj._values()  # 转存字典
+        # print(articleTypeList_dict, type(articleTypeList_dict))
+        # articleTypeList = list(articleTypeList_dict)
+        # print(articleTypeList, type(articleTypeList))
+        # return JsonResponse({'code': 200, 'info': '测试！'})
+
+        return render(request, 'test.html')
